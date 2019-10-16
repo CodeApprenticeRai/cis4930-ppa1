b700e2bd9d4781ce91dc2acf145a562fc03aca9f
Tare <tare.dgg@gmail.com>
2019-10-16 00:32:30 -0400

    added database solution caching and history printing for Retirement function

--- Retirement.py
+++ Retirement.py
@@ -1,14 +1,33 @@
 import sys
-
 sys.path.append(".")
 
-import re, pymongo, time, pprint
-
+import pymongo, time
 from utilities import isfloat
 
 class Retirement:
-    def command_line_routine(self):
+    def command_line_routine(self, db=None):
         print("You've selected the function: RETIREMENT")
+
+        print("\n\nHistory:")
+
+        if ( type(db) == type(None) ):
+            db_client = pymongo.MongoClient("mongodb://localhost:27017/")
+            db = db_client["function_solution_records"]
+
+        collection = db["records"]
+
+        # TODO: Clean this up.
+        print("Starting Age", "Salary", "Percentage Saved", "Savings Goal", "Solution", "Time", sep="\t")
+        for record in collection.find({ "function_name": "Retirement"}):
+            print(
+                record["parameters"]["starting_age"],
+                record["parameters"]["salary"],
+                record["parameters"]["percentage_saved"],
+                record["parameters"]["savings_goal"],
+                record["solution"], record["time"],
+                sep="\t" )
+
+        print( "*"*5, "End of History", "*"*5, "\n\n", sep="" )
 
         while (True):
             age = input("Enter your age: ")
@@ -49,9 +68,32 @@ class Retirement:
         print("Your retirement age will be {}\n\n".format(retirement_age))
         return None
 
-    def calculate_when_savings_goal_reached(self, age, salary, percentage_saved, savings_goal):
+    def calculate_when_savings_goal_reached(self, starting_age, salary, percentage_saved, savings_goal, db=None):
         capital = 0
+        age = starting_age
         while ( (age < 100) and (capital < savings_goal)):
             capital += salary * percentage_saved * 1.35
             age += 1
+
+        current_unix_time = int( time.time() )
+        if ( type(db) == type(None) ):
+            db_client = pymongo.MongoClient("mongodb://localhost:27017/")
+            db = db_client["function_solution_records"]
+
+        collection = db["records"]
+
+        data = {
+            "function_name": "Retirement",
+            "time": current_unix_time,
+            "parameters": {
+                "starting_age": starting_age,
+                "salary": salary,
+                "percentage_saved": percentage_saved,
+                "savings_goal": savings_goal
+            },
+            "solution": age
+        }
+
+        collection.insert_one(data)
+
         return age
