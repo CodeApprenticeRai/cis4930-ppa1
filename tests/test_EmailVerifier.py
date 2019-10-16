08a68224a73f0e7ca28d427e64d70c44380f4766
Tare <tare.dgg@gmail.com>
2019-10-16 00:13:46 -0400

    added database solution caching for EmailVerifier #2

--- tests/test_EmailVerifier.py
+++ tests/test_EmailVerifier.py
@@ -1,4 +1,4 @@
-import pytest
+import pytest, pymongo
 from .. import EmailVerifier
 
 '''
@@ -8,27 +8,31 @@ Conditions:
 '''
 
 # Case of empty string
-def test1(applet=EmailVerifier.EmailVerifier()):
-    test_result = applet.verify_email("")
+def test1(applet=EmailVerifier.EmailVerifier(), db=None):
+    test_result = applet.verify_email("", db)
     assert test_result  == False
 
 # Case of invalid email becuase of email non-domain portion of email starting with a '.'
-def test2(applet=EmailVerifier.EmailVerifier()):
-    test_result = applet.verify_email(".dfdsaf@gmail.com")
+def test2(applet=EmailVerifier.EmailVerifier(), db=None):
+    test_result = applet.verify_email(".dfdsaf@gmail.com", db)
     assert test_result == False
 
 # Case of invalid email becuase of email non-domain portion of email ending with a '.'
-def test3(applet=EmailVerifier.EmailVerifier()):
-    test_result = applet.verify_email("sdfsdklfj.@gmail.com")
+def test3(applet=EmailVerifier.EmailVerifier(), db=None):
+    test_result = applet.verify_email("sdfsdklfj.@gmail.com", db)
     assert test_result == False
 
 # Case of valid email
-def test4(applet=EmailVerifier.EmailVerifier()):
-    test_result = applet.verify_email("j.!$%*+-=?^_{|}~.smith@gmail.com")
+def test4(applet=EmailVerifier.EmailVerifier(), db=None):
+    test_result = applet.verify_email("j.!$%*+-=?^_{|}~.smith@gmail.com", db)
     assert test_result == True
 
 applet = EmailVerifier.EmailVerifier()
-test1(applet)
-test2(applet)
-test3(applet)
-test4(applet)
+
+db_client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = db_client["function_solution_records_mock"]
+
+test1(applet, db)
+test2(applet, db)
+test3(applet, db)
+test4(applet, db)
